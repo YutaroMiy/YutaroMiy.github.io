20531957784b1c1688bce9672f155efccf5d10e3
YutaroMiy <yutaromt636@gmail.com>
2020-06-29 18:37:01 -0400

    html survey file accessible by localhost

--- Module6/surveyController.js
+++ Module6/surveyController.js
@@ -1,7 +1,7 @@
 module.exports = function(app){
 	
 	app.get('/mySurvey', function(req, res){
-
+		res.sendFile(__dirname+'/views/mySurvey.html');
 	});
 
 	app.post('/mySurvey', function(req, res){
