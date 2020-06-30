7c72e18b6c3327c8866159c50960964a92cce5ca
YutaroMiy <yutaromt636@gmail.com>
2020-06-30 15:07:08 -0400

    Working storage

--- Module6/public/assets/action.js
+++ Module6/public/assets/action.js
@@ -1,11 +1,13 @@
 // jQuery that will "listen" to the html niceSurvey.html
 $(document).ready(function(){
 
-  $('form').on('submit', function(){
-      
-      // var item = $('form input');
-      // console.log(item.serializeArray());
+  $('form').on('submit', function(e){
+    e.preventDefault();  
 
+      // var item = $('form input');
+      console.log("Sending post request");
+      //console.log(item.serializeArray());
+  
       $.ajax({
         type: 'POST',
         url: '/mySurvey',
@@ -13,11 +15,12 @@ $(document).ready(function(){
         success: function(data){
           // do something with the data via front-end framework
           // Make the submit button red, disabled and saying Thank you
-          //$("#bb").css("background-color", "red");
-          //$("#bb").prop("disabled", "true");
-          //$("#bb").text("Thank you!");
+          $("#bb").css("background-color", "red");
+          $("#bb").prop("disabled", "true");
+          $("#bb").text("Thank you!");
         }
       });
       return false;
   });
 });
+
