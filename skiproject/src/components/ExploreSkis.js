fe5a44b073216d817e1bc29bed0c4c6e652f9169
YutaroMiy <yutaromt636@gmail.com>
2020-07-08 19:53:55 -0400

    finished explore ski page

--- skiproject/src/components/ExploreSkis.js
+++ skiproject/src/components/ExploreSkis.js
@@ -1,10 +1,91 @@
+/*
+	Rossignol AE taken from: https://www.sportchek.ca/categories/shop-by-sport/alpine-skiing/ski-packages/mens/product/rossignol-experience-74-xpress-2-mens-skis-2019-20-look-xpress-1-332899738.html#332899738%5Bcolor%5D=332899738_99
+	K2 Mindbender taken from: https://www.sportchek.ca/categories/shop-by-sport/alpine-skiing/flat-skis/mens/product/k2-mindbender-90c-mens-skis-2019-20-332927047.html#332927047%5Bcolor%5D=332927047_99
+*/
 import React from 'react'
 
 const ExploreSkis = () => {
 	return (
 		<div className="container">
 			<h3 className="center">Explore Skis</h3>
-			<p>Hiiiii</p>
+			<div className="row">
+  				<div className="column locations">
+  					<img src="/rossignolae.JPG" alt ="Rossignol AE" width="100%" height="300" />
+  					<h5 className="center">Rossignol 2020 AE Set</h5>
+  					<p className="center">Comes With Skis, Boots, Poles</p>
+  					<p className="center">Available Sizes: S,M,L</p>
+  					<p className="center">Available Boot Sizes: US Men's 6 - 14</p>
+  					<p className="center">$399.99</p>
+  				</div>
+  				<div className="column locations">
+  					<img src="/rossignolae.JPG" alt ="Rossignol AE" width="100%" height="300" />
+  					<h5 className="center">Rossignol 2020 AE (Skis Only)</h5>
+  					<p className="center">Skis Only</p>
+  					<p className="center">Available Sizes: S,M,L</p>
+  					<p className="center">$329.99</p>
+  					
+  				</div>
+  				<div className="column locations">
+  					<img src="/rossignolae.JPG" alt ="Rossignol AE" width="100%" height="300" />
+  					<h5 className="center">Rossignol 2020 AE (Skis & Boots)</h5>
+  					<p className="center">Skis & Boots Set.</p>
+  					<p className="center">Available Sizes: S,M,L</p>
+  					<p className="center">Available Boot Sizes: US Men's 6 - 14</p>
+  					<p className="center">$359.99</p>
+  				</div>
+			</div>
+			<div className="row">
+  				<div className="column locations">
+  					<img src="/k2mindbender.JPG" alt ="K2 Mindbender" width="100%" height="300" />
+  					<h5 className="center">K2 Mindbender Set</h5>
+  					<p className="center">Comes With Skis, Boots, Poles</p>
+  					<p className="center">Available Sizes: S,M,L</p>
+  					<p className="center">Available Boot Sizes: US Men's 7 - 12</p>
+  					<p className="center">$459.99</p>
+  				</div>
+  				<div className="column locations">
+  					<img src="/k2mindbender.JPG" alt ="K2 Mindbender" width="100%" height="300" />
+  					<h5 className="center">K2 Mindbender (Skis Only)</h5>
+  					<p className="center">Skis Only</p>
+  					<p className="center">Available Sizes: S,M,L</p>
+  					<p className="center">$379.99</p>
+  					
+  				</div>
+  				<div className="column locations">
+  					<img src="/k2mindbender.JPG" alt ="K2 Mindbender" width="100%" height="300" />
+  					<h5 className="center">K2 Mindbender (Skis & Boots)</h5>
+  					<p className="center">Skis & Boots Set.</p>
+  					<p className="center">Available Sizes: S,M,L</p>
+  					<p className="center">Available Boot Sizes: US Men's 7 - 12</p>
+  					<p className="center">$429.99</p>
+  				</div>
+			</div>
+			<div className="row">
+  				<div className="column locations">
+  					<img src="/atomicvantage.JPG" alt ="Atomic Vantage" width="100%" height="300" />
+  					<h5 className="center">Atomic Vantage Set</h5>
+  					<p className="center">Comes With Skis, Boots, Poles</p>
+  					<p className="center">Available Sizes: S,M,L</p>
+  					<p className="center">Available Boot Sizes: US Men's 2 - 9</p>
+  					<p className="center">$349.99</p>
+  				</div>
+  				<div className="column locations">
+  					<img src="/atomicvantage.JPG" alt ="Atomic Vantage" width="100%" height="300" />
+  					<h5 className="center">Atomic Vantage (Skis Only)</h5>
+  					<p className="center">Skis Only</p>
+  					<p className="center">Available Sizes: S,M,L</p>
+  					<p className="center">$269.99</p>
+  					
+  				</div>
+  				<div className="column locations">
+  					<img src="/atomicvantage.JPG" alt ="Atomic Vantage" width="100%" height="300" />
+  					<h5 className="center">Atomic Vantage (Skis & Boots)</h5>
+  					<p className="center">Skis & Boots Set.</p>
+  					<p className="center">Available Sizes: S,M,L</p>
+  					<p className="center">Available Boot Sizes: US Men's 6 - 14</p>
+  					<p className="center">$309.99</p>
+  				</div>
+			</div>
 		</div>
 	)
 }
